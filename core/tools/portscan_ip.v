92d68db4eb56c6dcf0eb097444c3792ea3a18730
social reject <94141577+9xN@users.noreply.github.com>
2022-04-19 16:27:17 -0600

    fix issues 1, 2 & 3 + return array

--- core/tools/portscan_ip.v
+++ core/tools/portscan_ip.v
@@ -1,20 +1,14 @@
-module portscan
+module tools
 
 import net
 
-pub fn portscan(ip string){
+pub fn portscan(ip string) []int {
+	mut ports := []int{}
 
 	for port := 0; port < 65535; port++ {
-		mut open_port_count := 0
-		mut closed_port_count := 0
-		mut conn := net.dial_tcp('${ip}:${port}') or {
-			closed_port_count += 1
-			continue
-		}
-		println('\x1b[31m[OPEN] \x1b[34m ${port} \x1b[39m \n')
-		open_port_count += 1
-		conn.close() or { return }
+		mut conn := net.dial_tcp('${ip}:${port}') or { continue }
+		ports << port
+		conn.close() or { continue }
 	}
-	println("$open_port_count open ports and $closed_port_count closed ones.")
+	return ports
 }
-
