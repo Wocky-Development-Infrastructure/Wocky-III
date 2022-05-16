a000c96daf49bd03aab43241e4b9a8af72eacb97
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-05-16 19:36:37 -0400

    Update gradient.v

--- core/term_control/gradient.v
+++ core/term_control/gradient.v
@@ -1,13 +1,13 @@
 module term_control
 
-pub fn gradiant(startrgb int, endrgb int, text string) {
-	changer = endrgb[0] - startrgb[0] / text.len
-	changeg = endrgb[1] - startrgb[1] / text.len
-	changeb = endrgb[2] - startrgb[2] / text.len
+pub fn gradiant(startrgb []int, endrgb []int, text string) {
+	changer := endrgb[0] - startrgb[0] / text.len
+	changeg := endrgb[1] - startrgb[1] / text.len
+	changeb := endrgb[2] - startrgb[2] / text.len
 	
-	r = startrgb[0]
-	g = startrgb[1]
-	b = startrgb[2]
+	mut r := startrgb[0]
+	mut g := startrgb[1]
+	mut b := startrgb[2]
 	
 	for letter in text {
 		println("\x1b[38;2;${r};${g};${b}m${letter}")
