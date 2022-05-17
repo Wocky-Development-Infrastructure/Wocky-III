aeaec489ff2ea8fb3aa0fcbfc32c1652fef3b3c1
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-05-16 22:25:17 -0400

    updates

--- tut.v
+++ tut.v
@@ -1,15 +1,18 @@
 import os
 import core.wockyfx
+import core.term_control
 
 
 fn main() {
-	mut wx := wx.parse_wfx{online_users: "root\nblueberry\nbrizy\nLamp\nVizionzx\nsocial\nfrosts\nmalware"}
-	args := os.args.clone()
-	if args.len < 2 {
-		println("[x] Error, Invalid argument. Usage: ${args[0]} <file.wfx>")
-		exit(0)
-	}
-	wx.parse_wfx(mut wx, args[1])
+	// mut wx := wx.parse_wfx{online_users: "root\nblueberry\nbrizy\nLamp\nVizionzx\nsocial\nfrosts\nmalware"}
+	// args := os.args.clone()
+	// if args.len < 2 {
+	// 	println("[x] Error, Invalid argument. Usage: ${args[0]} <file.wfx>")
+	// 	exit(0)
+	// }
+	// wx.parse_wfx(mut wx, args[1])
+
+	term_control.gradiant([24, 0, 142], [52, 5, 241], "test")
 
 	// println(wockyfx.get_callback_code("attack", "set_arg_err_msg"))
 }
